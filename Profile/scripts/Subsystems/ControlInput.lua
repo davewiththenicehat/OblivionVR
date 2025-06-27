41bb61b13a4cedf90a9c83332c895c41921c3a79
davewiththenicehat <54369722+davewiththenicehat@users.noreply.github.com>
2025-06-27 17:39:31 -0400

    button remapping working, config bug
    button remapping working.
    loading changes from config but not working yet.

--- Profile/scripts/Subsystems/ControlInput.lua
+++ Profile/scripts/Subsystems/ControlInput.lua
@@ -63,8 +63,8 @@ local controller_map_reference = {
     ["lThumb"]    = XINPUT_GAMEPAD_LEFT_THUMB,
     ["rThumb"]    = XINPUT_GAMEPAD_RIGHT_THUMB,
     ["Abutton"]   = XINPUT_GAMEPAD_A,
-    ["Bbutton"]   = XINPUT_GAMEPAD_X,  -- B and X are inverted
-    ["Xbutton"]   = XINPUT_GAMEPAD_B,  -- ^
+    ["Xbutton"]   = XINPUT_GAMEPAD_B,  -- B and X are inverted
+    ["Bbutton"]   = XINPUT_GAMEPAD_X,  -- ^
     ["Ybutton"]   = XINPUT_GAMEPAD_Y
 }
 
@@ -94,9 +94,9 @@ xinput_button_needing_press:16384
 local default_uevr_action_controller_map = {
     lThumb = "sprint",
     rThumb = "change view",
-    Ybutton = "jump", -- This is normally thumb right up
-    Xbutton = "crouch", -- This is normally thumb right down
-    Bbutton = "stow weapon",
+    Ybutton = "jump", -- This is right thumb up in the UEVR profile
+    Xbutton = "crouch", -- This is right thumb down in the UEVR profile
+    Bbutton = "stow weapon", 
     Abutton = "activate",
     lShoulder = "weapon quick menu",
     rShoulder = "cast spell",
@@ -115,22 +115,13 @@ end
 uevr.sdk.callbacks.on_xinput_get_state(
     function(retval, user_index, state)
 
-
-
-        
-
         --[[
-        for button_name, xinput_button_instance in pairs(controller_map_reference) do
-            if isButtonPressed(state, xinput_button_instance) then
-                print("\nButton pressed: "..button_name)
-            end
+        --load settings from the config table
+        for _, game_action_name in pairs(default_uevr_action_controller_map) do
+            controller_action_options[game_action_name] = config_table[game_action_name]
         end
-
-        if isButtonPressed(state, XINPUT_GAMEPAD_X) then
-            print ("controlleraction_options table:")
-            for action_name, button_name in pairs(controller_action_options) do
-                print (action_name..": "..button_name)
-            end
+        for key, value in pairs(controller_action_options) do
+            controller_button_to_actions_map[value] = key
         end
         ]]
 
@@ -145,128 +136,57 @@ uevr.sdk.callbacks.on_xinput_get_state(
 
         else -- game is playing, not in a game menu
 
-            local button_presses_needed = {}
+            local buttons_to_press = {}   -- To collect XInput constants of buttons that should be pressed
 
             -- Loop through all the buttons we are allowing players to remap
-            for button_name, xinput_button_instance in pairs(controller_map_reference) do 
+            for pressed_button_name, pressed_xinput_instance in pairs(controller_map_reference) do
                 -- The current button we are looping through has been pressed by the player.
-                if isButtonPressed(state, xinput_button_instance) then
-                    print("\nButton pressed: "..button_name)
-                    default_button_action = default_uevr_action_controller_map[button_name]
-                    local user_mapped_button_for_action = controller_action_options[default_button_action]
-                    print("default_button_action: "..default_button_action)
-                    print("user_mapped_button_for_action: "..user_mapped_button_for_action)
-                    -- If the user has mapped a different action for this button.
-                    if user_mapped_button_for_action ~= button_name then
-                        print("Unpressing: "..tostring(xinput_button_instance))
-                        unpressButton(state, xinput_button_instance) -- stop pressing the button
-                        -- Add an instance of the button the user would like this action mapped
-                        -- to the button_presses_needed table.
-                        -- Don't press the button now. The loop is not done.
-                        local button_player_wants_pressed = uevr_action_to_button_controller_map[button_name]
-                        print("button_player_wants_pressed: "..button_player_wants_pressed)
-                        table.insert(button_presses_needed, button_player_wants_pressed)
+                if isButtonPressed(state, pressed_xinput_instance) then
+
+                    -- Get the default action for this physically pressed button
+                    local default_action_for_button = default_uevr_action_controller_map[pressed_button_name]
+
+                    -- If there's a default action associated with this button
+                    if default_action_for_button then
+                        -- Get the user's chosen button NAME for this default action from CONFIG.lua
+                        action_player_wants = controller_button_to_actions_map[pressed_button_name]
+                        local user_mapped_button_name_for_action = controller_action_options[action_player_wants]
+
+                        -- Check if the user has remapped this action to a *different* button name
+                        if default_action_for_button ~= pressed_button_name then
+                            -- User has remapped it.
+                            -- 1. Unpress the physically pressed button
+                            for action_name, button_name in pairs(default_uevr_action_controller_map) do
+                                print ("\t"..action_name..": "..button_name)
+                            end
+                            unpressButton(state, pressed_xinput_instance)
+
+                            -- 2. Determine the XInput constant for the button the user *wants* pressed
+                            button_name_player_wants_pressed = uevr_action_to_button_controller_map[action_player_wants]
+                            local target_xinput_instance = controller_map_reference[button_name_player_wants_pressed]
+
+                            -- Make sure the target button is not "None" and is a valid XInput constant
+                            if target_xinput_instance and user_mapped_button_name_for_action ~= "None" then
+                                table.insert(buttons_to_press, target_xinput_instance)
+                            end
+                        end
                     end
                 end
             end
 
-            --if button_presses_needed then print("\nContents of button_presses_needed.") end
-            for key, value in pairs(button_presses_needed) do
-                print("\t"..tostring(key) .. ": " .. tostring(value))
+            -- Then, apply the presses
+            for _, xinput_to_press in ipairs(buttons_to_press) do
+                pressButton(state, xinput_to_press)
             end
 
-            --loop throug button_presses_needed and press the buttons
-            for _, xinput_button_needing_press in ipairs(button_presses_needed) do
-                print("xinput_button_needing_press:"..xinput_button_needing_press)
-                pressButton(state, xinput_button_needing_press)
-            end
-
-            
-            --[[
-            -- Check if the Y button is pressed and the Quick Menu is not currently active.
-            if Ybutton and QuickMenu == false then
-                -- Unpress the physical Y button to prevent its default action.
-                unpressButton(state, XINPUT_GAMEPAD_Y)
-                -- Trigger the "QuickMenuInput_Pressed" action on the player controller.
-                api:get_player_controller():QuickMenuInput_Pressed()
-                -- Set QuickMenu to true to indicate it's now active.
-                QuickMenu = true
-            -- If the Y button is not pressed.
-            elseif not Ybutton then
-                -- If the Quick Menu was previously active.
-                if QuickMenu == true then
-                    -- Trigger the "QuickMenuInput_Released" action on the player controller.
-                    api:get_player_controller():QuickMenuInput_Released()
-                    -- Set QuickMenu to false as it's no longer active.
-                    QuickMenu = false
-                end
-            end
-            
-            -- If the Y button is pressed (after the QuickMenu logic above, this might be a re-press or for other actions).
-            if Ybutton then
-                -- Unpress the physical Y button.
-                unpressButton(state, XINPUT_GAMEPAD_Y)
-                -- (Commented out: potentially would have pressed the Right Shoulder button)
-                --pressButton(state,XINPUT_GAMEPAD_RIGHT_SHOULDER)
-            end
-
-            -- If the B button is not pressed, set BbuttonNotPressedAfterMenu to true.
-            -- This acts as a flag to ensure the B button remapping only happens once after release.
-            if not Bbutton then
-                BbuttonNotPressedAfterMenu = true
-            end
-
-            -- If the X button is pressed.
-            if Xbutton then
-                -- (Commented out: XbuttonNotPressedAfterMenu was a potential flag, but not used)
-                --XbuttonNotPressedAfterMenu=true
-                -- Unpress the physical X button.
-                unpressButton(state, XINPUT_GAMEPAD_X)
-                -- Press the Right Shoulder button, effectively remapping X to Right Shoulder.
-                pressButton(state, XINPUT_GAMEPAD_RIGHT_SHOULDER)
-            end
-
-            -- If the Right Shoulder button is pressed.
-            if rShoulder then
-                -- Unpress the physical Right Shoulder button. This means the Right Shoulder's default action is consumed.
-                unpressButton(state, XINPUT_GAMEPAD_RIGHT_SHOULDER)
-            end
-
-            -- If the B button is pressed AND the BbuttonNotPressedAfterMenu flag is true.
-            if Bbutton and BbuttonNotPressedAfterMenu then
-                -- Unpress the physical B button.
-                unpressButton(state, XINPUT_GAMEPAD_B)
-                -- Press the X button, effectively remapping B to X.
-                pressButton(state, XINPUT_GAMEPAD_X)
-            end
-
-            -- If the Left Thumbstick button (L3) is pressed.
-            if lThumb then
-                -- Unpress the physical Left Thumbstick button.
-                unpressButton(state, XINPUT_GAMEPAD_LEFT_THUMB)
-                -- (Commented out: potentially would have pressed the Left Shoulder button)
-                --pressButton(state,XINPUT_GAMEPAD_LEFT_SHOULDER)
-            end
-
-            -- If the Left Shoulder button (L1 or Left Grip) is pressed.
-            if lShoulder then
-                -- Unpress the physical Left Shoulder button. This means the Left Shoulder's default action is consumed.
-                unpressButton(state, XINPUT_GAMEPAD_LEFT_SHOULDER)
-                -- Press the Left Thumbstick button, effectively remapping Left Shoulder to Left Thumbstick (often used for sprint).
-                pressButton(state, XINPUT_GAMEPAD_LEFT_THUMB)
-            end
-            ]]--
-
-            -- If the Right Thumbstick Y-axis is pushed significantly up.
+            -- if the right thumb pad was pressed (beyond the deadzone) up jump
             if ThumbRY > 30000 then
-                -- Press the Y button. This remaps a strong upward Right Thumbstick push to the Y button.
-                pressButton(state, XINPUT_GAMEPAD_Y)
+                pressButton(state, XINPUT_GAMEPAD_Y)  -- press the jump key
             end
 
-            -- If the Right Thumbstick Y-axis is pulled significantly down.
+            -- if the right thumb pad was pressed (beyond the deadzone) up jump
             if ThumbRY < -30000 then
-                -- Press the B button. This remaps a strong downward Right Thumbstick pull to the B button.
-                pressButton(state, XINPUT_GAMEPAD_B)
+                pressButton(state, XINPUT_GAMEPAD_B)  -- press the crouch key
             end
         end
 
